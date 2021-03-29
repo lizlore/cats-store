6773fee7ea2e5a42e75a8c5f4dd600b52c67c4df
Lorena <lore>
2021-03-29 16:08:06 -0600

    feat: apply lazy loading - add module share and core

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
-
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ExponentialPipe } from './pipes/exponential/exponential.pipe';
+import { HighlightDirective } from './directives/highlight/highlight.directive';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    ExponentialPipe,
+    HighlightDirective,
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent,
+    ExponentialPipe,
+    HighlightDirective,
+  ],
+  imports: [CommonModule, RouterModule],
 })
-export class SharedModule { }
+export class SharedModule {}
